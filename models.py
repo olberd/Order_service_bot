40a4c01708d8de6df085f04ccbcaa9ba193fd499
Nikolay Savchenko <7548568@gmail.com>
2022-11-23 20:13:30 +0300

    Add DB model

---
+++ models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class Client(models.Model):
+    name =  models.CharField('ФИО', max_length=200)
+	birthday = models.DateField('День рождения', null=True)
+    сontact = models.CharField('Телефон', max_length=200)
+	address = models.CharField('Адрес', max_length=200)
+	
+	
+class Сourier(models.Model):
+    name =  models.CharField('ФИО', max_length=200)
+    сontact = models.CharField('Телефон', max_length=200)
+	
+
+class Order(models.Model):
+    client = models.ForeignKey(Client)
+    date_time = models.DateField('Дата заказа') #Автоподстановка
+	cake = models.ForeignKey(Cake)
+	comment = models.CharField('Комментарий к заказу', max_length=200)
+	delivery_address = models.CharField('Адрес доставки', max_length=200) #По умолчанию адрес клиента
+	delivery_date = models.DateField('Дата доставки')
+	delivery_time = models.DateField('Желаемое время доставки')
+	payment # Заглушка 
+	
+	
+class Cake(models.Model):
+    number_of_levels
+	form
+	topping
+	berries
+	decor
+	inscription
+	price
+	
+	
+class Delivery(models.Model):
+    order = models.ForeignKey(Order)
+	courier = models.ForeignKey (Сourier)
+	delivery_time = models.DateField('Предполагаемое время доставки')
+	
+
