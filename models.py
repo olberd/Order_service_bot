bf0c7fbff0ba6ea2f07162e70f1fe1c12a16f1be
Nikolay Savchenko <7548568@gmail.com>
2022-11-23 22:38:51 +0300

    Update models

--- models.py
+++ models.py
@@ -1,39 +1,51 @@
 from django.db import models
 
+
 class Client(models.Model):
-    name =  models.CharField('ФИО', max_length=200)
+    name = models.CharField('ФИО', max_length=200)
     birthday = models.DateField('День рождения', null=True)
     contact = models.CharField('Телефон', max_length=200)
     address = models.CharField('Адрес', max_length=200)
 
 
 class Courier(models.Model):
-    name =  models.CharField('ФИО', max_length=200)
+    name = models.CharField('ФИО', max_length=200)
     contact = models.CharField('Телефон', max_length=200)
 
 
-class Order(models.Model):
-    client = models.ForeignKey(Client)
-    date_time = models.DateField('Дата заказа') #Автоподстановка
-    cake = models.ForeignKey(Cake)
-    comment = models.CharField('Комментарий к заказу', max_length=200)
-    delivery_address = models.CharField('Адрес доставки', max_length=200) #По умолчанию адрес клиента
-    delivery_date = models.DateField('Дата доставки')
-    delivery_time = models.DateField('Желаемое время доставки')
-    payment # Заглушка
-
-
 class Cake(models.Model):
-    number_of_levels = models.MultipleChoiceField(choices=['1 уровень', '2 уровня', '3 уровня'])
-    form = models.MultipleChoiceField(choices=['Квадрат', 'Круг', 'Прямоугольник'])
-    topping
+    levels = (
+        ('1 уровень', '1 уровень'),
+        ('2 уровня', '2 уровня'),
+        ('3 уровня', '3 уровня'),
+    )
+    number_of_levels = models.CharField(max_length=10, choices=levels)
+    forms = (
+        ('Квадрат', 'Квадрат'),
+        ('Круг', 'Круг'),
+        ('Прямоугольник', 'Прямоугольник'),
+    )
+    form = models.CharField(max_length=20, choices=forms)
+    topping =
     berries
     decor
     inscription
     price
 
 
+class Order(models.Model):
+    client = models.ForeignKey(Client)
+    date_time = models.DateTimeField(auto_now_add=True)
+    cake = models.ForeignKey(Cake)
+    comment = models.CharField('Комментарий к заказу', max_length=200)
+    delivery_address = models.CharField('Адрес доставки', max_length=200,
+                                        default=client__address)  # По умолчанию адрес клиента, не знаю как обратиться
+    delivery_date = models.DateField('Дата доставки')
+    delivery_time = models.TimeField('Желаемое время доставки')
+    payment = models.BooleanField(default=True)
+
+
 class Delivery(models.Model):
     order = models.ForeignKey(Order)
-    courier = models.ForeignKey (Сourier)
-    delivery_time = models.DateField('Предполагаемое время доставки')
+    courier = models.ForeignKey(Courier)
+    delivery_time = models.DateTimeField('Предполагаемое время доставки')
